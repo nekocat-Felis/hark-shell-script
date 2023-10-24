dec0ab7df1d48883a8596918eb6a1064db8ec8ef
nekocat0320 <131769192+nekocat0320@users.noreply.github.com>
2023-10-24 12:35:09 +0900

    commit

--- hark.sh
+++ hark.sh
@@ -1,4 +1,5 @@
-function installFunc(name) {
+function installFunc () {
+    name=$1
     getopts "denable:" denBool
     getopts "py" pyBool
     cd ${HOME}/hark
@@ -8,25 +9,28 @@ function installFunc(name) {
         python3 setup.py build
         sudo python3 setup.py install
         return 0
+    fi
+    ${ $(cat $name".c")//"#include <sys/io.h>"//"/* #include <sys/io.h>*/"} > $name".c"
     mkdir build
     cd build
     if [$denable = "?"] ; then
         cmake ..
     else
         cmake -DENABLE_RASP24=$denBool -DENABLE_WS=$denBool ..
+    fi
     make
     sudo make install
 }
 
 
 cd ${HOME}
-if [ $(ls | grep -o hark)"" = "hark" ] ; then
-    echo "harkディレクトリがないことを確認しました。"
-else
+if [ -d "hark" ] ; then
     echo "エラー：harkディレクトリが存在しています。"
     return 0
+else
+    echo "harkディレクトリがないことを確認しました。"
+fi
 
-START
 sudo apt install libtool cmake libxml2-dev libzip-dev libasound2-dev libopenblas-dev libgtk2.0-dev libsndfile1-dev libsdl2-dev liblapacke-dev gfortran python3-setuptools python3-dev libpopt-dev python3-daemon python3-paho-mqtt libmosquittopp-dev python3-pkgconfig python3-pybind11
 harkList=("hark-base" "libhark-netapi" "libharkio3" "hark-core" "harkmw" "hark-linux" "hark-gtkplot" "harktool5")
 optsList=("?" "?" "?" "denable=OFF" "py" "denable=OFF" "?" "?")
