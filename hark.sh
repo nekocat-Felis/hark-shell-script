a7135d765b21830e8819b70426ab0d4d79018ae1
nekocat0320 <131769192+nekocat0320@users.noreply.github.com>
2023-10-24 12:37:01 +0900

    commit

--- hark.sh
+++ hark.sh
@@ -29,12 +29,12 @@ if [ -d "hark" ] ; then
     return 0
 else
     echo "harkディレクトリがないことを確認しました。"
+    mkdir hark
 fi
 
-sudo apt install libtool cmake libxml2-dev libzip-dev libasound2-dev libopenblas-dev libgtk2.0-dev libsndfile1-dev libsdl2-dev liblapacke-dev gfortran python3-setuptools python3-dev libpopt-dev python3-daemon python3-paho-mqtt libmosquittopp-dev python3-pkgconfig python3-pybind11
+#sudo apt install libtool cmake libxml2-dev libzip-dev libasound2-dev libopenblas-dev libgtk2.0-dev libsndfile1-dev libsdl2-dev liblapacke-dev gfortran python3-setuptools python3-dev libpopt-dev python3-daemon python3-paho-mqtt libmosquittopp-dev python3-pkgconfig python3-pybind11
 harkList=("hark-base" "libhark-netapi" "libharkio3" "hark-core" "harkmw" "hark-linux" "hark-gtkplot" "harktool5")
 optsList=("?" "?" "?" "denable=OFF" "py" "denable=OFF" "?" "?")
-mkdir hark
 
 for ((i=0; i<"${#harkList[@]}"; i++)); do
     if [$optsList[i] = "py"] ; then
