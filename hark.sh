8ead84c14eb29639ef3fa9fcbaf0126077df80f9
nekocat0320 <131769192+nekocat0320@users.noreply.github.com>
2023-10-31 15:37:13 +0900

    commit

--- hark.sh
+++ hark.sh
@@ -46,8 +46,8 @@ cat /etc/apt/sources.list | sed s/"# deb-src"/deb-src/ | sed s/"#deb-src"/deb-sr
 
 # apt でインストールできるものを入れ、それ以外のリストを作成
 sudo apt install libtool cmake libxml2-dev libzip-dev libasound2-dev libopenblas-dev libgtk2.0-dev libsndfile1-dev libsdl2-dev liblapacke-dev gfortran python3-setuptools python3-dev libpopt-dev python3-daemon python3-paho-mqtt libmosquittopp-dev python3-pkgconfig python3-pybind11 -y
-harkList=("hark-base" "libhark-netapi" "libharkio3" "hark-core" "harkmw" "hark-linux" "hark-gtkplot" "harktool5")
-optsList=("" "" "" "denable=OFF" "py" "denable=OFF" "" "")
+harkList=("hark-base" "libhark-netapi" "libharkio3" "hark-core" "harkmw" "hark-linux" "hark-gtkplot" "harktool5" "harktool5-gui")
+optsList=("" "" "" "denable=OFF" "py" "denable=OFF" "" "" "py")
 
 for ((i=0; i<"${#harkList[@]}"; i++)); do
     if [ ${optsList[i]}"" = "py" ] ; then
