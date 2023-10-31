fe6b367b9772354a8d6bd450a58a7f59ebe561b7
nekocat0320 <131769192+nekocat0320@users.noreply.github.com>
2023-10-31 11:45:47 +0900

    commit

--- hark.sh
+++ hark.sh
@@ -41,7 +41,7 @@ else
 fi
 
 # apt でインストールできるものを入れ、それ以外のリストを作成
-sudo apt install libtool cmake libxml2-dev libzip-dev libasound2-dev libopenblas-dev libgtk2.0-dev libsndfile1-dev libsdl2-dev liblapacke-dev gfortran python3-setuptools python3-dev libpopt-dev python3-daemon python3-paho-mqtt libmosquittopp-dev python3-pkgconfig python3-pybind11
+sudo apt install libtool cmake libxml2-dev libzip-dev libasound2-dev libopenblas-dev libgtk2.0-dev libsndfile1-dev libsdl2-dev liblapacke-dev gfortran python3-setuptools python3-dev libpopt-dev python3-daemon python3-paho-mqtt libmosquittopp-dev python3-pkgconfig python3-pybind11 -y
 harkList=("hark-base" "libhark-netapi" "libharkio3" "hark-core" "harkmw" "hark-linux" "hark-gtkplot" "harktool5")
 optsList=("?" "?" "?" "denable=OFF" "py" "denable=OFF" "?" "?")
 
