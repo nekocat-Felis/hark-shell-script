8240c8ca6c8f8ed0671b8ce45852dd2e1386d111
nekocat0320 <131769192+nekocat0320@users.noreply.github.com>
2023-11-01 13:52:42 +0900

    commit

--- hark.sh
+++ hark.sh
@@ -56,7 +56,7 @@ harkList=("hark-base" "libhark-netapi" "libharkio3" "hark-core" "harkmw" "hark-l
 optsList=("" "" "" "denable=OFF" "py" "denable=OFF" "" "")
 
 for ((i=0; i<"${#harkList[@]}"; i++)); do
-    echo '[ ${optsList[i]}"" = "py" ]:' [ "${optsList[i]}" = "py" ]
+    echo 'option:' "${optsList[i]}"
     if [ ${optsList[i]}"" = "py" ] ; then
         installFunc ${harkList[i]} -py
     elif [[ "${optsList[i]}" =~ "denable" ]] ; then
