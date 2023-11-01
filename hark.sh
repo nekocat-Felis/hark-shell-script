8b546fe770fe0a93c651b7c3a984721c365e9190
nekocat0320 <131769192+nekocat0320@users.noreply.github.com>
2023-11-01 14:38:06 +0900

    commit

--- hark.sh
+++ hark.sh
@@ -49,7 +49,7 @@ else
     echo "harkディレクトリがないことを確認しました。"
     mkdir hark
 fi
-sed -i s/"# deb-src"/deb-src/ "/etc/apt/sources.list"
+cat /etc/apt/sources.list | sed s/"# deb-src"/deb-src/ | sed s/"#deb-src"/deb-src/ | sudo tee /etc/apt/sources.list
 
 # apt でインストールできるものを入れ、それ以外のリストを作成
 sudo apt install libtool cmake libxml2-dev libzip-dev libasound2-dev libopenblas-dev libgtk2.0-dev libsndfile1-dev libsdl2-dev liblapacke-dev gfortran python3-setuptools python3-dev libpopt-dev python3-daemon python3-paho-mqtt libmosquittopp-dev python3-pkgconfig python3-pybind11 -y
