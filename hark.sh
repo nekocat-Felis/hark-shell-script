a84a395f421ae890fefe7001fcd58c2b7ed1ea30
nekocat0320 <131769192+nekocat0320@users.noreply.github.com>
2023-11-01 13:58:20 +0900

    commit

--- hark.sh
+++ hark.sh
@@ -61,8 +61,7 @@ for ((i=0; i<"${#harkList[@]}"; i++)); do
     if [ ${optsList[i]}"" = "py" ] ; then
         installFunc ${harkList[i]} -py
     elif [[ "${optsList[i]}" =~ "denable" ]] ; then
-        echo ${optsList[i]##*=}
-        installFunc ${harkList[i]} -denable [${optsList[i]}##*=]
+        installFunc ${harkList[i]} -denable ${optsList[i]##*=}
     else
         echo else
         installFunc ${harkList[i]}
