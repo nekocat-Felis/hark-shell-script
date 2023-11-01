40b12b90577b9fb5b12202d0038db588e9ff50e0
nekocat0320 <131769192+nekocat0320@users.noreply.github.com>
2023-11-01 11:19:13 +0900

    commit

--- hark.sh
+++ hark.sh
@@ -44,10 +44,10 @@ else
     echo "harkディレクトリがないことを確認しました。"
     mkdir hark
 fi
-cat /etc/apt/sources.list | sed s/"# deb-src"/deb-src/ | sed s/"#deb-src"/deb-src/ | sudo tee "/etc/apt/sources.list"
+#cat /etc/apt/sources.list | sed s/"# deb-src"/deb-src/ | sed s/"#deb-src"/deb-src/ | sudo tee "/etc/apt/sources.list"
 
 # apt でインストールできるものを入れ、それ以外のリストを作成
-sudo apt install libtool cmake libxml2-dev libzip-dev libasound2-dev libopenblas-dev libgtk2.0-dev libsndfile1-dev libsdl2-dev liblapacke-dev gfortran python3-setuptools python3-dev libpopt-dev python3-daemon python3-paho-mqtt libmosquittopp-dev python3-pkgconfig python3-pybind11 -y
+#sudo apt install libtool cmake libxml2-dev libzip-dev libasound2-dev libopenblas-dev libgtk2.0-dev libsndfile1-dev libsdl2-dev liblapacke-dev gfortran python3-setuptools python3-dev libpopt-dev python3-daemon python3-paho-mqtt libmosquittopp-dev python3-pkgconfig python3-pybind11 -y
 harkList=("hark-base" "libhark-netapi" "libharkio3" "hark-core" "harkmw" "hark-linux" "hark-gtkplot" "harktool5" "harktool5-gui")
 optsList=("" "" "" "denable=OFF" "py" "denable=OFF" "" "" "py")
 
