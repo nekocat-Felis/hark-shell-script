ab3a64616df467241f63dc8f972b8fc3779f2247
nekocat0320 <131769192+nekocat0320@users.noreply.github.com>
2023-11-17 19:53:44 +0900

    commit

---
+++ hark-lib_docker.sh
@@ -0,0 +1,32 @@
+# home 上に harkディレクトリ が既に存在するか確認
+cd ${HOME}
+if [ -d "hark" ] ; then
+    echo "harkディレクトリが存在することを確認しました。"
+else
+    echo "警告:harkディレクトリが存在しません。"
+    mkdir hark
+fi
+cat /etc/apt/sources.list | sed s/"# deb-src"/deb-src/ | sed s/"#deb-src"/deb-src/ | sudo tee /etc/apt/sources.list
+apt update
+
+# apt install と pip3 でインストールできるものを入れ、それ以外のリストを作成
+apt install cmake-extras build-essential libopenblas-base liblapack-dev libeigen3-dev pybind11-dev python3-dev python3-pybind11 python3-setuptools python3-pkgconfig python3-pip python3-numpy python3-scipy python3-matplotlib python3-kivy libfmt-dev zlib1g-dev -y
+pip3 install --upgrade pip numpy setuptools pybind11 pkgconfig
+
+cd hark
+
+wget https://hark.jp/networks/hark-lib-1.0.0-20221215.tar.xz
+tar -Jxvf hark-lib-1.0.0-20221215.tar.xz
+
+
+cd hark-lib
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX:STRING=/usr
+make
+make install
+
+cd ..
+cd python
+python3 ./setup.py build
+python3 ./setup.py install
---
+++ hark_docker.sh
@@ -0,0 +1,73 @@
+function installFunc () {
+    # ファイル名とオプション
+    name=$1
+    denBool=""
+    pyBool=""
+    if [ $# -gt 1 ] ; then
+        case $2 in
+            -d)
+            denBool=$3;;
+            -p)
+            pyBool="py";;
+        esac
+    fi
+
+    # home/hark 上で作業する
+    cd ${HOME}/hark
+
+    # コードのダウンロードと作業ディレクトリへの移動
+    apt source $name
+    echo $( ls | grep -o $name"-.*" )
+    cd $( ls | grep -o $name"-.*" )
+    
+    # ビルドとインストール
+    if [ $pyBool"" = "py" ] ; then
+        python3 setup.py build
+        python3 setup.py install
+        return 0
+    fi
+    if [ $name"" = "libhark-netapi" ] ; then
+        sed -i 's/#include <sys\/io\.h>/\/\* #include <sys\/io\.h> \*\//g' hark-netapi.c
+    fi
+    mkdir build
+    cd build
+    if [ $denBool"" = "" ] ; then
+        cmake ..
+    else
+        cmake -DENABLE_RASP24=$denBool -DENABLE_WS=$denBool ..
+    fi
+    make
+    make install
+}
+
+
+# home 上に harkディレクトリ が既に存在するか確認
+cd ${HOME}
+if [ -d "hark" ] ; then
+    echo "警告:harkディレクトリが存在しています。"
+else
+    echo "harkディレクトリがないことを確認しました。"
+    mkdir hark
+fi
+cat /etc/apt/sources.list | sed s/"# deb-src"/deb-src/ | sed s/"#deb-src"/deb-src/ | sudo tee /etc/apt/sources.list
+apt update
+
+# apt install と pip3 でインストールできるものを入れ、それ以外のリストを作成
+apt install libtool cmake libxml2-dev libzip-dev libasound2-dev libopenblas-dev libgtk2.0-dev libsndfile1-dev libsdl2-dev liblapacke-dev gfortran python3-setuptools python3-dev libpopt-dev -y
+pip3 install --upgrade pip numpy setuptools
+apt install python3-daemon python3-paho-mqtt libmosquittopp-dev python3-pkgconfig python3-pybind11 -y
+pip3 install pybind11 pkgconfig paho-mqtt python-daemon soundfile
+
+harkList=("hark-base" "libhark-netapi" "libharkio3" "hark-core" "harkmw" "hark-linux" "hark-gtkplot" "harktool5")
+optsList=("" "" "" "denable=OFF" "py" "denable=OFF" "" "")
+
+for ((i=0; i<"${#harkList[@]}"; i++)); do
+    if [ ${optsList[i]}"" = "py" ] ; then
+        installFunc ${harkList[i]} -p
+    elif [[ "${optsList[i]}" =~ "denable" ]] ; then
+        installFunc ${harkList[i]} -d ${optsList[i]##*=}
+    else
+        echo else
+        installFunc ${harkList[i]}
+    fi
+done
