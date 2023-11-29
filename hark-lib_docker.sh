db52b32b5562b01a3466e7a88cb8d2d8e483d02f
nekocat0320 <131769192+nekocat0320@users.noreply.github.com>
2023-11-29 12:48:16 +0900

    commit

--- hark-lib_docker.sh
+++ hark-lib_docker.sh
@@ -10,7 +10,7 @@ cat /etc/apt/sources.list | sed s/"# deb-src"/deb-src/ | sed s/"#deb-src"/deb-sr
 apt update
 
 # apt install と pip3 でインストールできるものを入れ、それ以外のリストを作成
-apt install cmake-extras build-essential libopenblas-base liblapack-dev libeigen3-dev pybind11-dev python3-dev python3-pybind11 python3-setuptools python3-pkgconfig python3-pip python3-numpy python3-scipy python3-matplotlib python3-kivy libfmt-dev zlib1g-dev -y
+apt install cmake-extras build-essential libopenblas-base liblapack-dev libeigen3-dev pybind11-dev python3-dev python3-pybind11 python3-setuptools python3-pkgconfig python3-pip python3-numpy python3-scipy python3-matplotlib python3-kivy libfmt-dev zlib1g-dev wget -y
 pip3 install --upgrade pip numpy setuptools pybind11 pkgconfig
 
 cd hark
