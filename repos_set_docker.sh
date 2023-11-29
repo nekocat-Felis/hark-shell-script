6abbfe0fb1c106d0969f30e1ca9b408344b994d7
nekocat0320 <131769192+nekocat0320@users.noreply.github.com>
2023-11-29 12:27:55 +0900

    commit

--- repos_set_docker.sh
+++ repos_set_docker.sh
@@ -6,13 +6,13 @@ curl -sSL http://archive.hark.jp/harkrepos/public.gpg -o /usr/share/keyrings/har
 codename=$(lsb_release -cs)
 
 if [ "$(lsb_release -i | awk '{print $3}')" = "Ubuntu" ] ; then
-    bash -c 'echo -e "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hark-archive-keyring.asc] http://archive.hark.jp/harkrepos codename non-free\ndeb-src [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hark-archive-keyring.asc] http://archive.hark.jp/harkrepos codename non-free" > /etc/apt/sources.list.d/hark.list'
+    bash -c 'echo -e "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hark-archive-keyring.asc] http://archive.hark.jp/harkrepos $(lsb_release -cs) non-free\ndeb-src [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hark-archive-keyring.asc] http://archive.hark.jp/harkrepos $(lsb_release -cs) non-free" > /etc/apt/sources.list.d/hark.list'
     echo "成功"
-elif [ codename = "bullseye" ] ; then
+elif [ "$(lsb_release -cs)" = "bullseye" ] ; then
     bash -c 'echo -e "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hark-archive-keyring.asc] http://archive.hark.jp/harkrepos focal non-free\ndeb-src [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hark-archive-keyring.asc] http://archive.hark.jp/harkrepos focal non-free" > /etc/apt/sources.list.d/hark.list'
     echo "成功"
 else
-    read -p "Debian の "codename" に対応する Ubuntu のバージョン名を、半角英字で入力してください。" uName
+    read -p "Debian の $(lsb_release -cs) に対応する Ubuntu のバージョン名を、半角英字で入力してください。" uName
     bash -c 'echo -e "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hark-archive-keyring.asc] http://archive.hark.jp/harkrepos ${uName} non-free\ndeb-src [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hark-archive-keyring.asc] http://archive.hark.jp/harkrepos ${uName} non-free" > /etc/apt/sources.list.d/hark.list'
     echo "書き込み完了"
 fi
